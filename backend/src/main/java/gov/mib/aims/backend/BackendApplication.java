aea2859550d8976c6188ba80032b191645252dd0
Andryss <andr.krivosheev@gmail.com>
2026-05-30 23:11:18 +0300

    Add backend checkstyle and test report

--- backend/src/main/java/gov/mib/aims/backend/BackendApplication.java
+++ backend/src/main/java/gov/mib/aims/backend/BackendApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
+    /**
+     * Точка входа в приложение.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
